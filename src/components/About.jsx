f99a8ec59ad20c4cf889b918975432cb657bfb30
Huỳnh Khắc Tâm <tamhuynhkhac@gmail.com>
2023-11-04 20:15:47 +0700

    debug react/tilt

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { Tilt } from "/node_modules/.vite/deps/react-tilt.js";
+import { Tilt } from "react-tilt";
 
 import { styles } from "../styles";
 import { services } from "../constants";
